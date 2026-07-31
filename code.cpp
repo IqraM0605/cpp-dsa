53ba7d68c3be8b901e860132a137c1cafd816764
Iqra Mulla <iqramulla0605@gmail.com>
2026-07-31 22:17:11 +0530

    while and for loop

--- code.cpp
+++ code.cpp
@@ -5,12 +5,25 @@ int main()
 {
     int n = 20;
 
+    cout << "Even Numbers: ";
     for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            cout << i << " ";
+        }
+    }
+
+    cout << endl;
+
+    cout << "Odd Numbers: ";
+    for (int i = 1; i <= n; i++)
+    {
         if (i % 2 != 0)
         {
             cout << i << " ";
         }
-    cout << endl;
+    }
 
     return 0;
 }
