d3a190db4f58b7bd4305d072f2901a06c999b462
Iqra Mulla <iqramulla0605@gmail.com>
2026-08-02 11:58:09 +0530

    Added Nested Loop program

--- Conditionals_Loops/NestedLoop.cpp
+++ Conditionals_Loops/NestedLoop.cpp
@@ -6,7 +6,7 @@ int main()
     for (int i = 1; i <= n; i++)
     {
         int m = 20;
-        for (int i = 1; i <= m; i++)
+        for (int j = 1; j <= m; j++)
         {
             cout << "#";
         }
