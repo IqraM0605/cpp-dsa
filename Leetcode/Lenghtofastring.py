9362660815bbf7b6fe4784ae22a87b7081785657
Iqra Mulla <iqramulla0605@gmail.com>
2026-08-03 23:30:25 +0530

    Solved LeetCode 3 - Longest Substring Without Repeating Characters

--- Leetcode/Lenghtofastring.py
+++ Leetcode/Lenghtofastring.py
@@ -1,27 +1,45 @@
 class Solution:
     def lengthOfLongestSubstring(self, s):
-
         seen = set()
         left = 0
         maxLength = 0
 
         for right in range(len(s)):
-
             while s[right] in seen:
                 seen.remove(s[left])
                 left += 1
 
             seen.add(s[right])
-
             maxLength = max(maxLength, right - left + 1)
 
         return maxLength
 
 
+# -----------------------
+# Test Cases
+# -----------------------
+
 obj = Solution()
 
-print(obj.lengthOfLongestSubstring("abcabcbb"))
-print(obj.lengthOfLongestSubstring("bbbbb"))
-print(obj.lengthOfLongestSubstring("pwwkew"))
-print(obj.lengthOfLongestSubstring(""))
-print(obj.lengthOfLongestSubstring("abba"))
+print("Input: abcabcbb")
+print("Output:", obj.lengthOfLongestSubstring("abcabcbb"))
+
+print()
+
+print("Input: bbbbb")
+print("Output:", obj.lengthOfLongestSubstring("bbbbb"))
+
+print()
+
+print("Input: pwwkew")
+print("Output:", obj.lengthOfLongestSubstring("pwwkew"))
+
+print()
+
+print("Input: ''")
+print("Output:", obj.lengthOfLongestSubstring(""))
+
+print()
+
+print("Input: abba")
+print("Output:", obj.lengthOfLongestSubstring("abba"))
